7240343345e9ffa457df7d5a71c2c0cb96024540
Claus Pfisterer <claus@apochroma.ch>
2022-04-22 16:46:36 +0200

    turn joystick right, ptz gets faster but counterwise

--- arduino-ptz/arduino-ptz.ino
+++ arduino-ptz/arduino-ptz.ino
@@ -18,34 +18,93 @@ IPAddress gateway(10, 10, 10, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress myDns(8, 8, 8, 8);
 
-// TODO: Declare something depending on your application
+#define joyX A1 // blue x axis - PAN
+#define joyY A0 // yellow y axis - TILT
+#define joyZ A2 // white z axis - Zoom
+
 EthernetClient client;
 
 int    HTTP_PORT   = 80;
 String HTTP_METHOD = "GET";
 char   HOST_NAME[] = "10.10.10.100";
 String PATH_NAME   = "/-wvhttp-01-/control.cgi";
-String queryString = "?pan=right&pan.speed=200";;
-
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+ // start the Ethernet connection:
+  Serial.println("Initialize Ethernet with DHCP:");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip, myDns);
+  } else {
+    Serial.print("  DHCP assigned IP ");
+    Serial.println(Ethernet.localIP());
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.print("connecting to ");
+  Serial.print(HOST_NAME);
+  Serial.println("...");
+}
 
-  // initialize the Ethernet shield using the static IP address:
-  Ethernet.begin(mac, ip, myDns, gateway, subnet);
+void makeRequest(int pValue, int xValue, int yValue, int zValue)
+{
+    if(client.connect(HOST_NAME, HTTP_PORT))
+    {
+      Serial.println("Connected to server");
+      String queryString = "?pan=right&pan.speed=";
+    //Serial.print(queryString); Serial.println(xValue);
+      client.println(HTTP_METHOD + " " + PATH_NAME + queryString + xValue + " HTTP/1.1");
+      client.println("Host: " + String(HOST_NAME));
+      client.println("Connection: close");
+      client.println(); // end HTTP header
 
-  // TODO: initialize something depending on your application
-  // connect to web server on port 80:
-  if(client.connect(HOST_NAME, HTTP_PORT)) {
-    // if connected:
-    Serial.println("Connected to server");
-    // make a HTTP request:
-    // send HTTP header
-    client.println(HTTP_METHOD + " " + PATH_NAME + queryString + " HTTP/1.1");
-    client.println("Host: " + String(HOST_NAME));
-    client.println("Connection: close");
-    client.println(); // end HTTP header
+      while(client.connected())
+      {
+        if(client.available())
+        {
+         // read an incoming byte from the server and print it to serial monitor:
+         char c = client.read();
+         Serial.print(c);
+        }
+      }
+    }
+}
 
+void loop()
+{  
+  int xValue, yValue, zValue, pValue;
+  // put your main code here, to run repeatedly:
+  pValue = digitalRead(2); //Serial.print(" P:"); Serial.print(pValue);
+  xValue = analogRead(joyX); //Serial.print(" X:"); Serial.print(xValue);
+  yValue = analogRead(joyY); //Serial.print(" Y:"); Serial.print(yValue);
+  zValue = analogRead(joyZ); //Serial.print(" Z:"); Serial.print(zValue);
+  Serial.println();
+
+///////////////////////////
+  
+  if (xValue <= 400)
+  {
+    makeRequest( pValue, xValue, yValue, zValue );
+    //Serial.print("makeRequest with xValue: ");Serial.print(xValue);
+  }  
+
+ delay(500);
+ 
     while(client.connected()) {
       if(client.available()){
         // read an incoming byte from the server and print it to serial monitor:
@@ -53,16 +112,4 @@ void setup() {
         Serial.print(c);
       }
     }
-
-    // the server's disconnected, stop the client:
-    client.stop();
-    Serial.println();
-    Serial.println("disconnected");
-  } else {// if not connected:
-    Serial.println("connection failed");
-  }
-}
-
-void loop() {
-
 }
